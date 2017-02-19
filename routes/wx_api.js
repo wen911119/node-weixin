aeb00329e2d95d0d89b551d62816c2dea49c1033
wen911119 <wen911119@163.com>
2017-02-19 23:04:57 +0800

    变量名冲突

--- routes/wx_api.js
+++ routes/wx_api.js
@@ -6,11 +6,11 @@ var WechatAPI = require('wechat-api');
 var wxapi = new WechatAPI('wx59859d17d4f8bf42', 'be9853a7f2fc05d4610c47c23c496148');
 
 router.get('/getqrcode', function (req, res, next) {
-    wxapi.createTmpQRCode(10001, 504800, function (err, res) {
+    wxapi.createTmpQRCode(10001, 504800, function (err, result) {
         if(err){
              res.send('err');
         }else{
-            res.send(wxapi.showQRCodeURL(res.ticket));
+            res.send(wxapi.showQRCodeURL(result.ticket));
         }
     });
 
