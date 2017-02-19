7fce21807fedcb64f53bc10f1323b9845adbb30d
wen911119 <wen911119@163.com>
2017-02-19 22:52:19 +0800

    在回复里给出二维码地址

--- routes/wx_api.js
+++ routes/wx_api.js
@@ -6,11 +6,15 @@ var WechatAPI = require('wechat-api');
 var wxapi = new WechatAPI('wx59859d17d4f8bf42', 'be9853a7f2fc05d4610c47c23c496148');
 
 router.get('/getqrcode', function (req, res, next) {
-    wxapi.createTmpQRCode(10000, 504800, function (err, res) {
-        err || console.log(wxapi.showQRCodeURL(res.ticket));
+    wxapi.createTmpQRCode(10001, 504800, function (err, res) {
+        if(err){
+             res.send('err');
+        }else{
+            res.send(wxapi.showQRCodeURL(res.ticket));
+        }
     });
 
-    res.send('ok');
+   
 });
 
 module.exports = router;
