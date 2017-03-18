465fbcbe3baf1a79b399e634c2fd7ed2412e6538
wenjun <wen911119@163.com>
2017-03-19 02:06:29 +0800

    联调通过！

--- routes/wx_api.js
+++ routes/wx_api.js
@@ -6,9 +6,10 @@ var WechatAPI = require('wechat-api');
 var wxapi = new WechatAPI('wx59859d17d4f8bf42', 'be9853a7f2fc05d4610c47c23c496148');
 
 router.get('/getqrcode', function (req, res, next) {
-    wxapi.createTmpQRCode(10001, 504800, function (err, result) {
+    const param = req.query.code;
+    wxapi.createTmpQRCode(param, 504800, function (err, result) {
         if(err){
-             res.send('err');
+             res.send('');
         }else{
             res.send(wxapi.showQRCodeURL(result.ticket));
         }
