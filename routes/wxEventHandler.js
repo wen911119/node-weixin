07e3bdede8cb4ffae741a7aab8753b93856d911a
wenjun <wen911119@163.com>
2017-03-20 00:47:37 +0800

    改成永久二维码

--- routes/wxEventHandler.js
+++ routes/wxEventHandler.js
@@ -2,7 +2,8 @@
  * Created by admin on 2017/2/17.
  */
 var clickEventHandler = require("./wx_clickEventHandler.js");
-
+var WechatAPI = require('wechat-api');
+var fetch = require('node-fetch');
 function EventHandler(msg) {
     var res = "事件处理结果";
     switch (msg.Event) {
@@ -16,6 +17,22 @@ function EventHandler(msg) {
             res = "取消订阅";
             break;
         case 'SCAN':
+            let msgs = msg.EventKey.split('a');
+            fetch("http://1270.0.0.1:4040/api/device/bind", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: "openid="+msg.FromUserName+"&deviceid="+msgs[0]+"&appid="+msgs[1]+"&developerid="+msgs[2]
+            }).then(function (data) {
+                return data.json()
+            }).then(function(jsonData){
+                if(jsonData.status=='ok'){
+                    res = '绑定成功'
+                }else{
+                    res = '绑定失败，失败原因：'+jsonData.msg
+                }
+            });
             res = msg.EventKey;
             break;
         default:
--- routes/wx_api.js
+++ routes/wx_api.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var fetch = require('node-fetch');
 
-var WechatAPI = require('wechat-api');
 var wxapi = new WechatAPI('wx59859d17d4f8bf42', 'be9853a7f2fc05d4610c47c23c496148');
 
 router.get('/getqrcode', function (req, res, next) {
     const param = req.query.code;
-    wxapi.createTmpQRCode(param, 504800, function (err, result) {
+    console.log(param);
+    wxapi.createLimitQRCode(param, function (err, result) {
         if(err){
              res.send('');
         }else{
