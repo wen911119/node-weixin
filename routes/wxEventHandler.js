bdd7db4e9905cfaf5d2682946bd23588c1a142f2
wenjun <wen911119@163.com>
2017-03-29 02:02:57 +0800

    let 换成var

--- routes/wxEventHandler.js
+++ routes/wxEventHandler.js
@@ -6,7 +6,7 @@ var WechatAPI = require('wechat-api');
 var fetch = require('node-fetch');
 function EventHandler(msg) {
     var res = "事件处理结果";
-    let p = new Promise(function (resolve, reject) {
+    var p = new Promise(function (resolve, reject) {
         switch (msg.Event) {
             case "CLICK":
                 res = clickEventHandler(msg.EventKey, msg);
@@ -21,7 +21,7 @@ function EventHandler(msg) {
                 resolve(res);
                 break;
             case 'SCAN':
-                let msgs = msg.EventKey.split('a');
+                var msgs = msg.EventKey.split('a');
                 fetch("http://127.0.0.1:4040/api/device/bind", {
                     method: "POST",
                     headers: {
