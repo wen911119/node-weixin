502bcf109b00924ca3b027ba7ff182804deaa712
文君 <wen911119@163.com>
2017-04-24 11:01:45 +0800

    增加订阅时的绑定

--- routes/wxEventHandler.js
+++ routes/wxEventHandler.js
@@ -13,8 +13,25 @@ function EventHandler(msg) {
                 resolve(res);
                 break;
             case "subscribe":
-                res = "订阅";
-                resolve(res);
+                var deviceId = 'E'+msg.EventKey;
+                fetch("http://127.0.0.1:4040/api/users/bind", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: "openid=" + msg.FromUserName + "&deviceid=" + deviceId
+                }).then(function (data) {
+                    return data.json()
+                }).then(function (jsonData) {
+                    console.log(jsonData, 11111111);
+                    if (jsonData.status == 'ok') {
+                        res = '绑定成功'
+                    } else {
+                        res = '绑定失败，失败原因：' + jsonData.msg
+                        console.log(res, 2222221111);
+                    }
+                    resolve(res);
+                }).catch(e => reject(e));
                 break;
             case "unsubscribe":
                 res = "取消订阅";
